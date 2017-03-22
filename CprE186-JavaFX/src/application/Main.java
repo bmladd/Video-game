a4c961dd1ecef89508cf34b79421377c4a4f1a53
gamem <gamem@DESKTOP-URBRASU.student.iastate.edu>
2017-03-22 15:30:53 -0500

    Yo fixes bro

--- CprE186-JavaFX/src/application/Main.java
+++ CprE186-JavaFX/src/application/Main.java
@@ -2,7 +2,9 @@ package application;
 
 import javafx.animation.AnimationTimer;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Group;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
@@ -12,12 +14,17 @@ import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-	@Override
-	public void start(Stage primaryStage) {
-
-	}
-
 	public static void main(String[] args) {
 		launch(args);
+	}
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		
+			Parent root = FXMLLoader.load(getClass().getResource("Main.fxml"));
+			Scene scene = new Scene(root);
+			primaryStage.setTitle("Trial");
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		
 	}
 }
