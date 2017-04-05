5094008e2a80c644babd6fcce9907f151f317044
gamem <gamem@DESKTOP-URBRASU.student.iastate.edu>
2017-04-05 15:26:40 -0500

    Actual working game

--- CprE186-JavaFX/src/application/SwitchingScenes.java
+++ CprE186-JavaFX/src/application/SwitchingScenes.java
@@ -9,56 +9,201 @@ import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
-public class SwitchingScenes extends Application{
-	
+import java.util.ArrayList;
+
+import javafx.animation.AnimationTimer;
+import javafx.event.EventHandler;
+import javafx.scene.input.KeyEvent;
+
+public class SwitchingScenes extends Application {
+
+	AnimationTimer Gamer;
+	private static int Movement = 1;
 	public Stage window;
-	public Scene scene1, scene2;
-	public static void main(String[] args){
+	public Scene scene1, scene2, scene3;
+
+	public static void main(String[] args) {
 		launch(args);
 	}
-	
+
 	@Override
 	public void start(Stage primarystage) throws Exception {
 		window = primarystage;
-		//Button1
-		Button B = new Button("Tanner");
+
+		// Images
+		Image space = new Image("file:testing/Courtyard.png");
+		Image space2 = new Image("file:testing/Mountain.png");
+		Image TitleScreen = new Image("file:testing/TitleScreen.png");
+
+		// Button1Copy
+		Button BC = new Button("Quit");
+		BC.setLayoutX(0);
+		BC.setLayoutY(0);
+		BC.setOnAction(e -> {
+			
+		window.setScene(scene2);
+		
+		});
+		// Layout for animation:
+		Group root = new Group();
+		scene1 = new Scene(root);
+		primarystage.setScene(scene1);
+		Canvas canvas = new Canvas(1024, 768);
+		root.getChildren().add(canvas);
+		root.getChildren().add(BC);
+		GraphicsContext gc = canvas.getGraphicsContext2D();
+
+		// Animation Timer:
+		Image hero = new Image("file:testing/hero.png", 50, 64, true, false);
+		Image Enemy = new Image("file:testing/enemya.png");
+		final long startNanoTime = System.nanoTime();
+
+		ArrayList<String> input = new ArrayList<String>();
+
+		scene1.setOnKeyPressed(new EventHandler<KeyEvent>() {
+			public void handle(KeyEvent e) {
+				String code = e.getCode().toString();
+
+				// only add once... prevent duplicates
+				if (!input.contains(code))
+					input.add(code);
+			}
+		});
+
+		scene1.setOnKeyReleased(new EventHandler<KeyEvent>() {
+			public void handle(KeyEvent e) {
+				String code = e.getCode().toString();
+				input.remove(code);
+			}
+		});
+
+		boolean[] KeyLast = { false, false, false, false };
+		String[] Keys = { "W", "A", "S", "D", "E" };
+
+		int[] position = { 24, 24 };
+		Gamer = new AnimationTimer() {
+			public void handle(long currentNanoTime) {
+				double t = (currentNanoTime - startNanoTime) / 1000000000.0;
+
+				gc.clearRect(0, 0, 1024, 768);
+				gc.drawImage(space, 0, 0);
+
+				// going up
+				if (input.contains(Keys[0])) {
+					if (KeyLast[0] && (position[1] != 24)) {
+						position[1] = position[1] - Movement;
+					} else {
+						KeyLast[0] = true;
+
+					}
+
+				} else {
+					KeyLast[0] = false;
+				}
+				// going left
+				if (input.contains(Keys[1])) {
+					if ((KeyLast[1] && (position[0] != 24))) {
+
+						position[0] = position[0] - Movement;
+
+					} else {
+						KeyLast[1] = true;
+
+					}
+
+				} else {
+					KeyLast[1] = false;
+				}
+				// going down
+				if (input.contains(Keys[2])) {
+					if (KeyLast[2] && (position[1] != 680)) {
+						position[1] = position[1] + Movement;
+					} else {
+						KeyLast[2] = true;
+
+					}
+
+				} else {
+					KeyLast[2] = false;
+				}
+				// going right
+				if (input.contains(Keys[3])) {
+					if (KeyLast[3] && (position[0] != 949)) {
+
+						position[0] = position[0] + Movement;
+
+					} else {
+						KeyLast[3] = true;
+
+					}
+
+				} else {
+					KeyLast[3] = false;
+				}
+
+				gc.drawImage(hero, position[0], position[1]);
+
+				// clear the canvas
+				// Background image clears canvas
+
+			}
+		};
+		//
+
+		// Button1
+		Button B = new Button("Quit");
+		B.setLayoutX(0);
+		B.setLayoutY(0);
 		B.setOnAction(e -> window.setScene(scene2));
 		
-		//Layout 1
-		Group root = new Group();
-		Scene scene1 = new Scene(root);
-		window.setScene(scene1);
-		Canvas canvas = new Canvas( 1024, 500 );
-		root.getChildren().add( canvas );
-		root.getChildren().add( B );
-		GraphicsContext gc = canvas.getGraphicsContext2D();
-		
-		
-		Image TitleScreen = new Image("file:TitleScreen.png");
-		gc.drawImage(TitleScreen, 0, 0);
-		
-		//Button2
-		Button B2 = new Button("I dont like tanner");
-		B2.setOnAction(e -> primarystage.setScene(scene1));
-		
-		//Layout 2
+
+		// Button3
+		Button B3 = new Button("Options");
+		B3.setLayoutX(430);
+		B3.setLayoutY(580);
+		B3.setPrefSize(160, 60);
+		B3.setOnAction(e -> window.setScene(scene3));
+		// Button2
+		Button B2 = new Button("Play");
+		B2.setLayoutX(430);
+		B2.setLayoutY(500);
+		B2.setPrefSize(160, 60);
+		B2.setOnAction(e -> {
+
+			window.setScene(scene1);
+			Gamer.start();
+
+		});
+
+		// Big Main Game Start//
+
+		// Big Main Game End//
+
+		// Layout3
+		Group root3 = new Group();
+		Canvas canvas3 = new Canvas(1024, 768);
+		root3.getChildren().add(canvas3);
+		root3.getChildren().add(B3);
+		root3.getChildren().add(B);
+		GraphicsContext gc3 = canvas3.getGraphicsContext2D();
+		gc3.drawImage(space2, 0, 0);
+		scene3 = new Scene(root3);
+
+		// Layout2
 		Group root2 = new Group();
-		Scene scene2 = new Scene(root2);
-		window.setScene(scene2);
-		Canvas canvas2 = new Canvas( 1024, 500 );
-		root2.getChildren().add( canvas2 );
-		root2.getChildren().add( B2 );
+		Canvas canvas2 = new Canvas(1024, 768);
+		root2.getChildren().add(canvas2);
+		root2.getChildren().add(B2);
+		root2.getChildren().add(B3);
 		GraphicsContext gc2 = canvas2.getGraphicsContext2D();
-		Image space = new Image( "file:Courtyard.png" );
-		gc2.drawImage(space, 0, 0);
-		
-		//end stuff
-		
-		window.setScene(scene1);
-		window.setTitle("Why Tanner?");
+		gc2.drawImage(TitleScreen, 0, 0);
+		scene2 = new Scene(root2);
+
+		// end stuff
+
+		window.setScene(scene2);
+		window.setTitle("Welcome!");
 		window.show();
 	}
-
-
 
 }
