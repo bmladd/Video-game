9145e4bd1a5831d352948569f012d92b11f83e0e
Brandon Ladd <bmladd@iastate.edu>
2017-04-05 15:40:31 -0500

    Edits to AI character, spawns lasers...

--- CprE186-JavaFX/src/application/AIcharacter.java
+++ CprE186-JavaFX/src/application/AIcharacter.java
@@ -3,41 +3,18 @@ package application;
 import java.util.ArrayList;
 import java.util.List;
 
-import javafx.animation.AnimationTimer;
-import javafx.application.Application;
-import javafx.stage.Stage;
 import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 
 
-public class AIcharacter extends Application {
+public class AIcharacter  {
 	private Pane root;
 	
 	private List<Node> enemies = new ArrayList<>();
-	private AnimationTimer timer;
-	
-	private Parent createContent() {
-			 root = new Pane();
-			 root.setPrefSize(1920, 1080);
-			 
-			 
-			 timer = new AnimationTimer() {
-				 @Override
-				 public void handle(long now) {
-					 onUpdate();
-				 }
-			 };
-			
-			 timer.start();
-			return root;
-			
-			
-	}
-	private Node spawnCharacter(){
+
+	public Node spawnLaser(){
 		Rectangle enemy = new Rectangle(160,340, Color.RED);
 		enemy.setTranslateY((int)(Math.random()*14)*40);
 		
@@ -45,35 +22,14 @@ public class AIcharacter extends Application {
 		return enemy;
 		
 	}
-	private void onUpdate() {
+	public void onUpdate(){
 		for(Node enemy : enemies)
 			enemy.setTranslateX(enemy.getTranslateX()+Math.random()*10);
 		if(Math.random() < 0.045) {
-			enemies.add(spawnCharacter());
+			enemies.add(spawnLaser());
 		}
-		checkState();
 		
 	}
 	
-	private void checkState() {
-		for(Node enemy : enemies) {
-			if(enemy.getBoundsInParent().intersects(sides.getBoundsInParent())) {
-				enemy.setTranslateX(650);
-				enemy.setTranslateY(875);
-				return;
-			}
-		}
-	}
 	
-	@Override
-	public void start(Stage scene) throws Exception {
-		scene.setScene(new Scene(createContent()));
-		
-		scene.show();// TODO Auto-generated method stub
-		
-	}
-	
-	public static void main(String[] args) {
-		launch(args);
-	}
 }
